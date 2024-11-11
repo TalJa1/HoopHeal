ba133e79f68cc5328cf58aa0aa7214f3b80decee
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-11-11 16:05:14 +0700

    Add signInStarIcon SVG and update SignIn2 header with logo and welcome message

--- views/login/SignIn2.tsx
+++ views/login/SignIn2.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable react-native/no-inline-styles */
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {containerStyle} from '../../services/styleProps';
+import {containerStyle, vh, vw} from '../../services/styleProps';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {signInStarIcon} from '../../assets/svgIcon';
 
 const SignIn2 = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
         <View style={{flex: 1}}>
+          <Header />
           <Text>SignIn2</Text>
         </View>
       </ScrollView>
@@ -16,8 +18,43 @@ const SignIn2 = () => {
   );
 };
 
+const Header: React.FC = () => {
+  return (
+    <View style={styles.header}>
+      <View style={styles.headerhorizontal}>
+        <Image source={require('../../assets/login/logo.png')} />
+        {signInStarIcon(vw(10), vw(10))}
+      </View>
+      <View style={styles.welcomeGrp}>
+        <Text style={styles.welcome}>Welcome</Text>
+        <Image source={require('../../assets/login/waving.png')} />
+      </View>
+    </View>
+  );
+};
+
 export default SignIn2;
 
 const styles = StyleSheet.create({
   container: containerStyle,
+  header: {
+    paddingHorizontal: vw(5),
+    paddingVertical: vh(2),
+  },
+  headerhorizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  welcomeGrp: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: vh(3),
+    columnGap: vw(5),
+  },
+  welcome: {
+    fontSize: 28,
+    fontWeight: '700',
+    color: '#BABABA',
+  },
 });
