75c3d6eaa378eea5d91b018abcd45d4fb768be3e
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-11-11 11:17:29 +0700

    Add main application structure with bottom tab navigation and initial screens

--- App.tsx
+++ App.tsx
@@ -1,118 +1,103 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StyleSheet, View} from 'react-native';
+import {vh, vw} from './services/styleProps';
+import Home from './views/bottoms/Home';
+import Exercise from './views/bottoms/Exercise';
+import ProgressView from './views/bottoms/ProgressView';
+import Settings from './views/bottoms/Settings';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  exerciseIcon,
+  homeIcon,
+  profileIcon,
+  progressIcon,
+} from './assets/svgIcon';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+const App = () => {
+  const TabNavigator = () => {
+    return (
+      <View style={styles.tabnavigationStyle}>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarActiveTintColor: '#F87643',
+            tabBarShowLabel: false,
+            tabBarStyle: {
+              borderTopColor: 'black',
+              backgroundColor: '#000000',
+              height: vh(8),
+            },
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => {
+                const iconSize = focused ? vw(7) : vw(6);
+                return <View>{homeIcon(iconSize, iconSize, color)}</View>;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Exercise"
+            component={Exercise}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => {
+                const iconSize = focused ? vw(7) : vw(6);
+                return <View>{exerciseIcon(iconSize, iconSize, color)}</View>;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="ProgressView"
+            component={ProgressView}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => {
+                const iconSize = focused ? vw(7) : vw(6);
+                return <View>{progressIcon(iconSize, iconSize, color)}</View>;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color, focused}) => {
+                const iconSize = focused ? vw(7) : vw(6);
+                return <View>{profileIcon(iconSize, iconSize, color)}</View>;
+              },
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+    );
+  };
+  return (
+    <NavigationContainer>
+      {/* Main || Login */}
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          component={TabNavigator}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  tabnavigationStyle: {backgroundColor: '#221E3D', flex: 1},
+});
---
+++ views/bottoms/ProgressView.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ProgressView = () => {
+  return (
+    <View>
+      <Text>ProgressView</Text>
+    </View>
+  );
+};
+
+export default ProgressView;
+
+const styles = StyleSheet.create({});
