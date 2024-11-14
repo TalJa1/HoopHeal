fcc88586a64c7ac7ea1c0b56f95227b5349b2805
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-11-14 15:31:07 +0700

    Refactor Exercise component to use SafeAreaView and ScrollView for improved layout and scrolling behavior

--- views/bottoms/Exercise.tsx
+++ views/bottoms/Exercise.tsx
@@ -1,14 +1,23 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {containerStyle, scrollContainer} from '../../services/styleProps';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Exercise = () => {
   return (
-    <View>
-      <Text>Exercise</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={scrollContainer}>
+        <View style={{flex: 1}}>
+          <Text>Exercise</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default Exercise;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: containerStyle,
+});
