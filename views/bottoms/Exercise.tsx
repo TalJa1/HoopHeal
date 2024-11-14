bc8beaa3837d4170e33995a62c84599126c9df9f
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-11-14 15:52:41 +0700

    Refactor Exercise component to include a header with notification icon and improve layout using SafeAreaView and ScrollView

--- views/bottoms/Exercise.tsx
+++ views/bottoms/Exercise.tsx
@@ -1,18 +1,33 @@
 /* eslint-disable react-native/no-inline-styles */
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {containerStyle, scrollContainer} from '../../services/styleProps';
+import {
+  containerStyle,
+  scrollContainer,
+  vh,
+  vw,
+} from '../../services/styleProps';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {notiIcon} from '../../assets/svgIcon';
 
 const Exercise = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={scrollContainer}>
         <View style={{flex: 1}}>
-          <Text>Exercise</Text>
+          <Header />
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerExer}>Exercise</Text>
+      {notiIcon(vw(7), vw(7), '#F87643')}
+    </View>
   );
 };
 
@@ -20,4 +35,16 @@ export default Exercise;
 
 const styles = StyleSheet.create({
   container: containerStyle,
+  header: {
+    paddingHorizontal: vw(5),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: vh(2),
+    alignItems: 'center',
+  },
+  headerExer: {
+    color: 'white',
+    fontSize: 28,
+    fontFamily: 'RacingSansOne-Regular',
+  },
 });
