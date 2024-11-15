3abcd9d02293174e32b25f9b7d8a6f6ce267b9d5
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-11-15 13:07:55 +0700

    Add back navigation and header image to Workout component

--- views/exercise/Workout.tsx
+++ views/exercise/Workout.tsx
@@ -1,12 +1,28 @@
 /* eslint-disable react-native/no-inline-styles */
-import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {containerStyle, scrollContainer} from '../../services/styleProps';
+import {
+  containerStyle,
+  scrollContainer,
+  vh,
+  vw,
+} from '../../services/styleProps';
 import {WorkoutDetailRouteProp} from '../../services/typeProps';
-import {useRoute} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {backArrowIcon} from '../../assets/svgIcon';
 
 const Workout = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
   const route = useRoute<WorkoutDetailRouteProp>();
   const {selectedIndex} = route.params;
 
@@ -17,6 +33,17 @@ const Workout = () => {
       <StatusBar barStyle="dark-content" backgroundColor={'white'} />
       <ScrollView contentContainerStyle={scrollContainer}>
         <View style={{flex: 1}}>
+          <View>
+            <TouchableOpacity
+              style={styles.headerBtn}
+              onPress={() => navigation.goBack()}>
+              {backArrowIcon(vw(7), vw(7), 'black')}
+            </TouchableOpacity>
+            <Image
+              style={styles.headerImg}
+              source={require('../../assets/exercise/woDetail.png')}
+            />
+          </View>
           <Text>Workout</Text>
         </View>
       </ScrollView>
@@ -28,4 +55,18 @@ export default Workout;
 
 const styles = StyleSheet.create({
   container: containerStyle,
+  headerBtn: {
+    position: 'absolute',
+    top: vh(3),
+    left: vw(5),
+    zIndex: 1,
+    backgroundColor: '#BABABA',
+    padding: 8,
+    borderRadius: vw(50),
+  },
+  headerImg: {
+    width: '100%',
+    height: vh(30),
+    resizeMode: 'cover',
+  },
 });
