979e59a7cf2bbc014675157c31fe95cb63754ae4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-11-15 13:48:47 +0700

    Add header to Calendar screen with navigation and icons

--- views/exercise/Calendar.tsx
+++ views/exercise/Calendar.tsx
@@ -1,8 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
-import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {containerStyle, scrollContainer} from '../../services/styleProps';
+import {
+  containerStyle,
+  marginHorizontal,
+  rowCenter,
+  scrollContainer,
+  vh,
+} from '../../services/styleProps';
+import {backArrowIcon, calendarIcon} from '../../assets/svgIcon';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
 
 const Calendar = () => {
   return (
@@ -10,10 +26,24 @@ const Calendar = () => {
       <StatusBar barStyle="light-content" backgroundColor={'black'} />
       <ScrollView contentContainerStyle={scrollContainer}>
         <View style={{flex: 1}}>
-          <Text>Calendar</Text>
+          <Header />
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  return (
+    <View
+      style={[marginHorizontal, rowCenter, {justifyContent: 'space-between', marginVertical: vh(2)}]}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        {backArrowIcon(30, 30, 'white')}
+      </TouchableOpacity>
+      <Text style={styles.headerTitle}>Calender</Text>
+      {calendarIcon(30, 30, '#F87643')}
+    </View>
   );
 };
 
@@ -21,4 +51,10 @@ export default Calendar;
 
 const styles = StyleSheet.create({
   container: containerStyle,
+  headerTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
 });
